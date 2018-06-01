e2cc8c3e894f2874cbf440b56cdb0229784e0a5a
Filip Leski <filtyc@gmail.com>
2018-06-01 17:18:54 -0400

    refactor to a more modern javascript and emit signals in place of speak and listen functions

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 'use strict';
 
-var Alexa = require('alexa-sdk');
+const Alexa = require('alexa-sdk');
 
 var handlers = {
   'LaunchRequest': function() {
-    this.response.speak("Hello, Welcome to Food Box! What would you like to cook?").listen("What would you like to cook?");
+    const welcomeMessage = 'Hello, Welcome to Food Box!';
+    const recipeQuestion = 'What would you like to cook?';
+
+    this.emit(':ask', `${welcomeMessage} ${recipeQuestion}`, recipeQuestion);
     this.emit(':responseReady');
   },
   'ChooseRecipe': function () {
-    var recipe = this.event.request.intent.slots.recipe.value;
-    this.response.speak("You chose " + recipe);
+    const chosenRecipe = this.event.request.intent.slots.recipe.value;
+
+    this.emit(':tell', `You chose ${chosenRecipe}.`);
     this.emit(':responseReady');
   }
 };
 
 exports.handler = function(event, context, callback){
-  var alexa = Alexa.handler(event, context);
+  const alexa = Alexa.handler(event, context);
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
