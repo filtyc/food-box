a139c8932aada3db1569fabad7b05dcd227cde27
Filip Leski <filtyc@gmail.com>
2018-06-02 18:11:36 -0400

    add a Repeat handler; start saving attributes between sessions in DynamoDB; different intros for first-time and repeat users

--- index.js
+++ index.js
@@ -7,16 +7,26 @@ let promptQueue = [];
 
 var handlers = {
   'LaunchRequest': function() {
-    this.attributes.foodBox = {
-      'currentRecipe': {},
-      'currentIngredient': 0,
-      'currentStep': 0
-    };
-
-    const welcomeMessage = 'Hello, Welcome to Food Box!';
+    promptQueue = [];
+    let welcomeMessage = '';
     const recipeQuestion = 'What would you like to cook?';
 
-    this.emit(':ask', `${welcomeMessage} ${recipeQuestion}`, recipeQuestion);
+    if (Object.keys(this.attributes).length === 0) {
+      this.attributes.foodBox = {
+        'currentRecipe': {},
+        'currentIngredient': 0,
+        'currentStep': 0
+      };
+
+      welcomeMessage = 'Welcome to Food Box: an Alexa skill that can read Blue Apron recipes for you. After you choose what you would like to cook you can keep asking for the next step, or for the current step to be repeated, until the recipe is finished. If you need to pause, simply say stop. Your progress will be saved.';
+    } else if (Object.keys(this.attributes.foodBox.currentRecipe).length !== 0) {
+      promptQueue.push(`You were cooking ${this.attributes.foodBox.currentRecipe.fullName} and your last step was: `);
+      this.emitWithState('Repeat');
+    } else {
+      welcomeMessage = 'Welcome back!';
+    }
+
+    this.emit(':ask', `${welcomeMessage} ${recipeQuestion}`);
     this.emit(':responseReady');
   },
   'ChooseRecipe': function () {
@@ -29,13 +39,13 @@ var handlers = {
       promptQueue = [];
       promptQueue.push(`You chose ${chosenRecipe}. `);
 
-      this.emitWithState('NextStep');
+      this.emitWithState('Next');
     } else {
       this.emit(':tell', `Sorry, I don't have a recipe for that.`);
       this.emit(':responseReady');
     }
   },
-  'NextStep': function () {
+  'Next': function () {
     const currentRecipe = this.attributes.foodBox.currentRecipe;
     let currentIngredient = this.attributes.foodBox.currentIngredient;
     let currentStep = this.attributes.foodBox.currentStep;
@@ -53,7 +63,11 @@ var handlers = {
       promptQueue.push(`${currentRecipe.steps[currentStep]}`);
       this.attributes.foodBox.currentStep++;
     } else {
-      promptQueue.push('There are no more steps for this recipe. Bye!');
+      this.attributes.foodBox.currentRecipe = {};
+      this.attributes.foodBox.currentIngredient = 0;
+      this.attributes.foodBox.currentStep = 0;
+
+      promptQueue.push('There are no more steps for this recipe. Goodbye!');
     }
 
     let message = promptQueue.join(' ');
@@ -61,11 +75,38 @@ var handlers = {
 
     this.emit(':ask', message);
     this.emit(':responseReady');
+  },
+  'Repeat': function () {
+    const currentRecipe = this.attributes.foodBox.currentRecipe;
+    let currentIngredient = this.attributes.foodBox.currentIngredient;
+    let currentStep = this.attributes.foodBox.currentStep;
+
+    if (currentIngredient !== 0 || currentStep !== 0) {
+      if (currentIngredient < currentRecipe.ingredients.length || currentStep === 0) {
+        this.attributes.foodBox.currentIngredient--;
+      } else {
+        this.attributes.foodBox.currentStep--;
+      }
+    }
+
+    this.emitWithState('Next');
+  },
+  'AMAZON.StopIntent': function() {
+    this.emit(':tell', 'Goodbye!');
+    this.emit(':responseReady');
+  },
+  'AMAZON.CancelIntent': function() {
+    this.emit(':tell', 'Goodbye!');
+    this.emit(':responseReady');
+  },
+  'SessionEndedRequest': function() {
+    this.emit(':saveState', true);
   }
 };
 
-exports.handler = function(event, context, callback){
+exports.handler = function(event, context, callback) {
   const alexa = Alexa.handler(event, context);
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  alexa.dynamoDBTableName = 'FoodBox';
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
