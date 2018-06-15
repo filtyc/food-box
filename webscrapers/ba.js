0bab879029a1c58798b043105d1306003c5d00ae
Filip Leski <filtyc@gmail.com>
2018-06-14 22:34:25 -0400

    deal with async better; save values to be pasted to the model in a separate file

--- webscrapers/ba.js
+++ webscrapers/ba.js
@@ -4,77 +4,102 @@ const rp = require('request-promise-native');
 const cheerio = require('cheerio');
 
 let recipeUrls = [];
+let recipeNames = [];
 let recipes = {};
-let rpOptions = {
-  uri: 'https://www.blueapron.com/pages/sample-recipes',
-  transform: (body) => cheerio.load(body),
+let options = {transform: body => cheerio.load(body)};
+let modelValues = '';
+
+
+const getRecipeUrls = async () => {
+  try {
+    const $ = await rp(options);
+
+    $('a.recipe-card').attr('href', (i, e) => {
+      if (i < 8) recipeUrls.push('https://www.blueapron.com' + e);
+    });
+  }
+  catch(err) {
+    console.log(err);
+  }
 };
 
-rp(rpOptions).then(($) => {
+const fetchRecipe = async () => {
+  let name = "";
+  let steps = [];
+  let lastIngredientIndex = 0;
+  let imageChangeIndexes = [];
+  let imageURLs = [];
 
-  $('a.recipe-card').attr('href', (i, e) => {
-    if (i < 8) {
-      recipeUrls.push('https://www.blueapron.com' + e);
-    }
-  });
+  try {
+    const $ = await rp(options);
 
-  recipeUrls.forEach((recipeUrl) => {
+    name = $('h1.ba-recipe-title__main').text().split('\n')[1].trim();
+    recipeNames.push(name);
 
-    rpOptions.uri = recipeUrl;
-
-    let name = "";
-    let steps = [];
-    let lastIngredientIndex = 0;
-    let imageChangeIndexes = [];
-    let imageURLs = [];
-
-    rp(rpOptions).then(($) => {
-
-      name = $('h1.ba-recipe-title__main').text().split('\n')[1].trim();
-
-      // ingredients
-      $('li[itemprop="ingredients"]').text((i, e) => {
-        steps.push(e.replace(/(\n)/gm, ' ').replace(/(\s\s+)/gm, ' ').trim() + '.');
-      })
-
-      lastIngredientIndex = steps.length - 1;
-
-      // steps
-      $('div.step-txt > p').text((i, e) => {
-        for (let step of e.split('.')) {
-          step = step.trim();
-          if (step) {
-            steps.push(step + '.');
-          }
-        }
-        imageChangeIndexes.push(steps.length);
-      })
-
-      imageChangeIndexes.pop();
-
-      // images
-      $('div.col-md-6.col-xs-12 > img').attr('src', (i, e) => {
-        imageURLs.push(e);
-      });
-
-
-      recipes[name] = {
-        name,
-        mealkit: 'Blue Apron',
-        steps,
-        lastIngredientIndex,
-        imageURLs,
-        imageChangeIndexes
-      };
-
-      fs.writeFileSync('output/ba-recipes.json', JSON.stringify(recipes, null, 2));
-
-    }).catch((err) => {
-      console.log(err);
+    // ingredients
+    $('li[itemprop="ingredients"]').text((i, e) => {
+      steps.push(e.replace(/(\n)/gm, ' ').replace(/(\s\s+)/gm, ' ').trim() + '.');
     });
 
-  });
+    lastIngredientIndex = steps.length - 1;
 
-}).catch((err) => {
-  console.log(err);
-});
+    // steps
+    $('div.step-txt > p').text((i, e) => {
+      for (let step of e.split('.')) {
+        step = step.trim();
+        if (step) {
+          steps.push(step + '.');
+        }
+      }
+      imageChangeIndexes.push(steps.length);
+    });
+
+    imageChangeIndexes.pop();
+
+    // images
+    $('div.col-md-6.col-xs-12 > img').attr('src', (i, e) => {
+      imageURLs.push(e);
+    });
+
+    recipes[name] = {
+      name,
+      mealkit: 'Blue Apron',
+      steps,
+      lastIngredientIndex,
+      imageURLs,
+      imageChangeIndexes
+    };
+  }
+  catch(err) {
+    console.log(err);
+  }
+};
+
+const getModelValues = () => {
+  for (const recipeName of recipeNames) {
+    modelValues +=
+`{
+    "name": {
+        "value": "${recipeName}"
+    }
+},
+`;
+  }
+};
+
+scrape = async () => {
+  options.uri = 'https://www.blueapron.com/pages/sample-recipes';
+  await getRecipeUrls();
+
+  for (const recipeUrl of recipeUrls) {
+    options.uri = recipeUrl;
+    await fetchRecipe();
+  }
+
+  getModelValues();
+
+  fs.writeFileSync('output/ba-recipes.json', JSON.stringify(recipes, null, 2));
+  fs.writeFileSync('output/ba-modelValues.txt', modelValues);
+};
+
+scrape();
